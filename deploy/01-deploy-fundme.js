029653f37a5df922e0539bd1cd6d6eb9eb5edc40
khushbusandhu <khushbusandhu12@gmail.com>
2024-01-20 10:13:02 +0530

    hardhat

---
+++ deploy/01-deploy-fundme.js
@@ -0,0 +1,14 @@
+const { networkConfig } = require("../helper-hardhat-config");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+};
+const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+
+const fundMe = await deploy("FundMe", {
+  from: deployer,
+  args: [address],
+  log: true,
+});
